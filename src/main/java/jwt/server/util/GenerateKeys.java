91f055eaf61bb5d77c53da47694521adff19cf43
daudmammaev <ffefef386@gmail.com>
2024-11-11 01:54:36 +0300

    Update GenerateKeys.java

--- src/main/java/jwt/server/util/GenerateKeys.java
+++ src/main/java/jwt/server/util/GenerateKeys.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
 
+import java.util.Arrays;
+
 public class GenerateKeys {
 
     public static void main(String[] args) {
@@ -15,5 +17,5 @@ public class GenerateKeys {
     private static String generateKey() {
         return Encoders.BASE64.encode(Keys.secretKeyFor(SignatureAlgorithm.HS512).getEncoded());
     }
-
+    
 }
